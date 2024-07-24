bf0271eedc8e965cba1110ba232e9b48b5836797
Jatin Vidhani <131987761+jatin-vidhani17@users.noreply.github.com>
2024-07-24 12:49:30 +0530

    Update JA1PR4.java

--- JA1PR4.java
+++ JA1PR4.java
@@ -45,26 +45,36 @@ class sequence{
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int choice = 0;
+        int choice = 0, num =0;
         do{
             menu();
             choice = scan.nextInt();
             scan.nextLine();
-	    System.out.print("Enter the Number Till You Want Numbers of Fibonaaci Sequence : ");
-            int num = scan.nextInt();
-            scan.nextLine();
-	    switch(choice){
+            switch(choice){
                 case 1:
+                    System.out.print("Enter the Number Till You Want Numbers of Fibonaaci Sequence : ");
+                    num = scan.nextInt();
+                    scan.nextLine();
                     System.out.print("The Fibonacci Sequence using For Loop : ");
                     useForLoop(num);
+                    System.out.println();
                     break;
                 case 2:
+                    System.out.print("Enter the Number Till You Want Numbers of Fibonaaci Sequence : ");
+                    num = scan.nextInt();
+                    scan.nextLine();
                     System.out.print("The Fibonacci Sequence using While Loop : ");
                     useWhileLoop(num);
+                    System.out.println();
                     break;
                 case 3:
+                    System.out.print("Enter the Number Till You Want Numbers of Fibonaaci Sequence : ");
+                    num = scan.nextInt();
+                    scan.nextLine();
                     System.out.print("The Fibonacci Sequence using Recurrsion : ");
                     useRecurrsion(num,0,1);
+                    System.out.println();
+                    System.out.println();
                     break;
                 case 4:
                     System.exit(0);
@@ -74,6 +84,44 @@ class sequence{
             }
         }
         while(choice!=4);
-        scan.close();
     }
 }
+/*
+
+Output : 
+
+---------------MENU----------------
+(1) Using For Loop.
+(2) Using While Loop.
+(3) Using Recurrsion.
+(4) Exit
+Enter the Choice : 1
+Enter the Number Till You Want Numbers of Fibonaaci Sequence : 11
+The Fibonacci Sequence using For Loop : 0 1 1 2 3 5 8 13 21 34 55
+
+---------------MENU----------------
+(1) Using For Loop.
+(2) Using While Loop.
+(3) Using Recurrsion.
+(4) Exit
+Enter the Choice : 2
+Enter the Number Till You Want Numbers of Fibonaaci Sequence : 15
+The Fibonacci Sequence using While Loop : 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377
+
+---------------MENU----------------
+(1) Using For Loop.
+(2) Using While Loop.
+(3) Using Recurrsion.
+(4) Exit
+Enter the Choice : 3
+Enter the Number Till You Want Numbers of Fibonaaci Sequence : 20
+The Fibonacci Sequence using Recurrsion : 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181
+
+---------------MENU----------------
+(1) Using For Loop.
+(2) Using While Loop.
+(3) Using Recurrsion.
+(4) Exit
+Enter the Choice : 4
+
+*/
