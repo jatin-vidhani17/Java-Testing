65a38bc668cafbef33cb49393a932575027301e5
Jatin Vidhani <131987761+jatin-vidhani17@users.noreply.github.com>
2024-09-24 12:51:52 +0530

    Update Q7.java

--- Assignement 2/Q7.java
+++ Assignement 2/Q7.java
@@ -1,4 +1,5 @@
-// Write an application that reads and processes strings from the console. Perform the following functions based on the menu choice selected by the user..
+// Write an application that reads and processes strings from the console.
+// Perform the following functions based on the menu choice selected by the user..
 //  Reverse the sequence of strings and then display it.
 //  Reverse the sequence of characters in each string and then display it.
 //  rearrange the strings according to the length of the string.
@@ -41,7 +42,7 @@ class mutableString{
     }
 
     void lengthSort() {
-        String[] words = str.split(" ");
+        String[] words = str.split("\\s+");
         for (int i = 0; i < words.length - 1; i++) {
             for (int j = i + 1; j < words.length; j++) {
                 if (words[i].length() > words[j].length()) {
@@ -58,14 +59,35 @@ class mutableString{
         str = str.trim();
     }
 
-    void alphaSort(){
+    void alphaSort() {
         String[] words = this.str.split("\\s+");
-        for (int i = 0; i < words.length-1; i++) {
-            for (int j = i; j < words.length-1; j++) {
-                
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = 0; j < words.length - 1 - i; j++) {
+                // Compare adjacent words and swap if they are in the wrong order
+                if (words[j].compareTo(words[j + 1]) > 0) {
+                    String temp = words[j];
+                    words[j] = words[j + 1];
+                    words[j + 1] = temp;
+                }
             }
         }
+        str = String.join(" ", words);
     }
+
+    void caseChange(){
+        System.out.print("\n\nChoose case change option: (1) Uppercase, (2) Lowercase : ");
+        int choice = scan.nextInt();
+        scan.nextLine(); // Consume the newline character
+
+        if (choice == 1) {
+            str = str.toUpperCase();
+        } else if (choice == 2) {
+            str = str.toLowerCase();
+        } else {
+            System.out.println("Invalid choice. No changes made.");
+        }
+    }
+
 
     void charReverse() {
         String[] words = str.split("\\s+");
@@ -83,23 +105,189 @@ class mutableString{
  
     public static void main(String[] args) {
         mutableString str1 = new mutableString();
-        String str,str_temp;
+        String str;
+
         System.out.print("Enter Valid Strings : ");
         str = str1.scan.nextLine();
         str1.setString(str);
-        str1.sequenceReverse();
-        System.out.println("\nReversed String : " + str1.getStrings());
-        str1.charReverse();
-        System.out.println("\nReversed Character String : " + str1.getStrings());
-        str1.lengthSort();
-        System.out.println("\nSorted According to Length of the String : " + str1.getStrings());
-        System.out.print("\nEnter The Postion where You want To Add String : \n(0) Front of Previous String.\n(1) Back of Previous String.\nEnter Position : ");
-        int pos = str1.scan.nextInt();
-        System.out.print("\nEnter the string you want to add : ");
-        str1.scan.nextLine();
-        str_temp = str1.scan.nextLine();
-        // str1.scan.nextLine();
-        str1.concat(str_temp, pos);
-        System.out.print("\nConacatenated String : " + str1.getStrings());
+
+        int choice;
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Reverse the sequence of strings");
+            System.out.println("2. Reverse the characters in each string");
+            System.out.println("3. Sort according to length of strings");
+            System.out.println("4. Sort alphabetically");
+            System.out.println("5. Concatenate a string");
+            System.out.println("6. Change case (upper/lower)");
+            System.out.println("7. Exit");
+            System.out.print("Enter your choice: ");
+            choice = str1.scan.nextInt();
+            str1.scan.nextLine(); // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    str1.sequenceReverse();
+                    System.out.println("Reversed String: " + str1.getStrings());
+                    break;
+                case 2:
+                    str1.charReverse();
+                    System.out.println("Reversed Character String: " + str1.getStrings());
+                    break;
+                case 3:
+                    str1.lengthSort();
+                    System.out.println("Sorted According to Length of the String: " + str1.getStrings());
+                    break;
+                case 4:
+                    str1.alphaSort();
+                    System.out.println("Alphabetically Sorted String: " + str1.getStrings());
+                    break;
+                case 5:
+                    System.out.print("Enter the position where you want to add the string (0 for front, 1 for back): ");
+                    int pos = str1.scan.nextInt();
+                    System.out.print("Enter the string you want to add: ");
+                    str1.scan.nextLine(); // Consume the newline character
+                    String str_temp = str1.scan.nextLine();
+                    str1.concat(str_temp, pos);
+                    System.out.println("Concatenated String: " + str1.getStrings());
+                    break;
+                case 6:
+                    str1.caseChange();
+                    System.out.println("String after case change: " + str1.getStrings());
+                    break;
+                case 7:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 7);
     }
 }
+
+/*
+
+Output : 
+
+Enter Valid Strings : Hello There!
+
+Menu:
+1. Reverse the sequence of strings
+2. Reverse the characters in each string
+3. Sort according to length of strings
+4. Sort alphabetically
+5. Concatenate a string
+6. Change case (upper/lower)
+7. Exit
+Enter your choice: 1
+Reversed String: There! Hello 
+
+Menu:
+1. Reverse the sequence of strings
+2. Reverse the characters in each string
+3. Sort according to length of strings
+4. Sort alphabetically
+5. Concatenate a string
+6. Change case (upper/lower)
+7. Exit
+Enter your choice: 1
+Reversed String: Hello There! 
+
+Menu:
+1. Reverse the sequence of strings
+2. Reverse the characters in each string
+3. Sort according to length of strings
+4. Sort alphabetically
+5. Concatenate a string
+6. Change case (upper/lower)
+7. Exit
+Enter your choice: 2
+Reversed Character String: olleH !erehT
+
+Menu:
+1. Reverse the sequence of strings
+2. Reverse the characters in each string
+3. Sort according to length of strings
+4. Sort alphabetically
+5. Concatenate a string
+6. Change case (upper/lower)
+7. Exit
+Enter your choice: 2
+Reversed Character String: Hello There!
+
+Menu:
+1. Reverse the sequence of strings
+2. Reverse the characters in each string
+3. Sort according to length of strings
+4. Sort alphabetically
+5. Concatenate a string
+6. Change case (upper/lower)
+7. Exit
+Enter your choice: 3
+Sorted According to Length of the String: Hello There!
+
+Menu:
+1. Reverse the sequence of strings
+2. Reverse the characters in each string
+3. Sort according to length of strings
+4. Sort alphabetically
+5. Concatenate a string
+6. Change case (upper/lower)
+7. Exit
+Enter your choice: 4
+Alphabetically Sorted String: Hello There!
+
+Menu:
+1. Reverse the sequence of strings
+2. Reverse the characters in each string
+3. Sort according to length of strings
+4. Sort alphabetically
+5. Concatenate a string
+6. Change case (upper/lower)
+7. Exit
+Enter your choice: 5
+Enter the position where you want to add the string (0 for front, 1 for back): 1 
+Enter the string you want to add: General Kenobi!
+Hello There! General Kenobi!
+Concatenated String: Hello There! General Kenobi!
+
+Menu:
+1. Reverse the sequence of strings
+2. Reverse the characters in each string
+3. Sort according to length of strings
+4. Sort alphabetically
+5. Concatenate a string
+6. Change case (upper/lower)
+7. Exit
+Enter your choice: 6
+
+
+Choose case change option: (1) Uppercase, (2) Lowercase : 1
+String after case change: HELLO THERE! GENERAL KENOBI!
+
+Menu:
+1. Reverse the sequence of strings
+2. Reverse the characters in each string
+3. Sort according to length of strings
+4. Sort alphabetically
+5. Concatenate a string
+6. Change case (upper/lower)
+7. Exit
+Enter your choice: 6
+
+
+Choose case change option: (1) Uppercase, (2) Lowercase : 2
+String after case change: hello there! general kenobi!
+
+Menu:
+1. Reverse the sequence of strings
+2. Reverse the characters in each string
+3. Sort according to length of strings
+4. Sort alphabetically
+5. Concatenate a string
+6. Change case (upper/lower)
+7. Exit
+Enter your choice: 7
+Exiting...
+
+*/
